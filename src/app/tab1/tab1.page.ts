920f6c857c9f8b89eeb4f09cd9a53547ab48ad0b
edinson salamanca <sennin7k@gmail.com>
2019-09-19 17:03:41 -0500

    Diseño de las interfacez

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+    posts = {
+        user: {
+            name: 'Ruben Elias',
+            email: 'ruben@gmail.com',
+            avatar: {
+                url: '/assets/avatars/av-1.png'
+            }
+        },
+        photo: {
+            url: '/assets/perro-2.jpg'
+        }
+    };
 
-  constructor() {}
+    constructor() {
+    }
 
 }
