d156101d30630349457acbbe479dcc159e95cb7c
edinson salamanca <sennin7k@gmail.com>
2019-11-05 14:17:45 -0500

    se agrego una nueva importacón llamada pipesModule

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,21 +1,14 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {PostsComponent} from './posts/posts.component';
-import {PostComponent} from './post/post.component';
-import {IonicModule} from '@ionic/angular';
-import {AvatarSelectorComponent} from './avatar-selector/avatar-selector.component';
-
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
+import { IonicModule } from '@ionic/angular';
+import { AvatarSelectorComponent } from './avatar-selector/avatar-selector.component';
+import { PipesModule } from '../pipes/pipes.module';
 
 @NgModule({
-    declarations: [PostsComponent, PostComponent, AvatarSelectorComponent],
-    exports: [
-        PostsComponent,
-        AvatarSelectorComponent
-    ],
-    imports: [
-        CommonModule,
-        IonicModule
-    ]
+  declarations: [PostsComponent, PostComponent, AvatarSelectorComponent],
+  exports: [PostsComponent, AvatarSelectorComponent],
+  imports: [CommonModule, IonicModule, PipesModule]
 })
-export class ComponentsModule {
-}
+export class ComponentsModule {}
