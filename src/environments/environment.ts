54708eb11b26f66956d3dac3396d1bdc25250a1e
edinson salamanca <sennin7k@gmail.com>
2019-11-09 01:30:15 -0500

    api key agregada

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'ban tus datos',
-        authDomain: 'ban tus datos',
-        databaseURL: 'ba tus datos',
-        projectId: '',
-        storageBucket: '',
-        messagingSenderId: '',
-        appId: 'ba tus datos'
+        apiKey: 'AIzaSyCD5U43-kyu9N0VuTIWUORbZmCAG9vcnmU',
+        authDomain: 'jetgram-f6ab9.firebaseapp.com',
+        databaseURL: 'https://jetgram-f6ab9.firebaseio.com',
+        projectId: 'jetgram-f6ab9',
+        storageBucket: 'jetgram-f6ab9.appspot.com',
+        messagingSenderId: '328548939889',
+        appId: '1:328548939889:web:74f28a5854eedbf47943d0'
     }
 };
 
